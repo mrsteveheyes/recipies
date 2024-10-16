a7338c60a9638990e519545ccacab0d0b18d5e7e
Steve Heyes <mrsteveheyes@gmail.com>
2024-10-16 12:23:08 +0100

    feat: dashboard and svg inline gem

--- Gemfile
+++ Gemfile
@@ -60,3 +60,4 @@ group :test do
 end
 
 gem "devise", "~> 4.9"
+gem "inline_svg"
--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,5 @@ Rails.application.routes.draw do
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
   # Defines the root path route ("/")
-  root to: "home#index"
+  root to: "dashboard#index"
 end
