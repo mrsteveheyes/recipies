ca9755c74a01eb13d50286a79549e25267002802
Steve Heyes <mrsteveheyes@gmail.com>
2025-05-27 20:55:01 +0100

    add: basic Family set up

---
+++ app/models/family.rb
@@ -0,0 +1,5 @@
+class Family < ApplicationRecord
+  has_many :users, dependent: :restrict_with_error
+
+  validates :name, presence: true
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_10_16_082845) do
+ActiveRecord::Schema[7.2].define(version: 2025_05_27_195103) do
+  create_table "families", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -19,7 +25,11 @@ ActiveRecord::Schema[7.2].define(version: 2024_10_16_082845) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "family_id", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["family_id"], name: "index_users_on_family_id"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "users", "families"
 end
